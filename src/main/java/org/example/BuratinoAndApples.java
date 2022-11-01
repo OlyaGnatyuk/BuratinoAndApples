33ec63fdb91e88f6ec9c97a692e766db5787e13a
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-01 14:36:31 +0300

    Initial commit

---
+++ src/main/java/org/example/BuratinoAndApples.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class BuratinoAndApples {
+    public static void main(String[] args) {
+        int buratinoApplesNumber = 8;
+        int karabasApplesNumber = 5;
+        int remainingApplesNumber = buratinoApplesNumber - karabasApplesNumber;
+    }
+}
